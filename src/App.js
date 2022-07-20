02f3ea13ff94e3bfd2f9d1d1d96176241de6a19c
FerrariRoma <win9592@gmail.com>
2022-07-21 02:29:44 +0900

    Update: initialize reducer state at login process

--- src/App.js
+++ src/App.js
@@ -3,14 +3,41 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Upload from "./pages/Upload";
+import { useEffect, useState } from "react";
+import { auth } from "./shared/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useDispatch, useSelector } from "react-redux";
+import { FBloadedAction } from "./redux/modules/userSlice";
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  const dispatch = useDispatch();
+
+  const loginCheck = async (user) => {
+    if (user) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  };
+  useEffect(() => {
+    onAuthStateChanged(auth, loginCheck);
+  }, [auth]);
+
+  useEffect(() => {
+    dispatch(FBloadedAction(auth.currentUser));
+  });
+  const result = useSelector((state) => console.log(state));
+  console.log(result);
+
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/upload" element={<Upload />} />
       </Routes>
     </div>
   );
